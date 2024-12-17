e270b192c3eb98760dcb9eed0998f98202c6ed26
Paulo Zanluqui <paulozanluqui13@gmail.com>
2024-12-16 22:25:42 -0300

    fix: Ajuste nas rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../pages/login";
 import Home from "../pages/home";
 import Register from "../pages/register";
-import ListReservations from "../pages/reservations/list-reservations";
 
 export const AppRouter = () => {
   return (
@@ -11,9 +10,6 @@ export const AppRouter = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/home" element={<Home />} />
-      <Route path="/reservations">
-        <Route path="list" element={<ListReservations />} />
-      </Route>
     </Routes>
   );
 };
