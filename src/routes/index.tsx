5b3057a766d6092d4e6ac0bdafa972079fa03217
Paulo Zanluqui <paulozanluqui13@gmail.com>
2024-12-30 20:58:52 -0300

    feat: Redirect da rota / para o login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "../pages/login";
 import Home from "../pages/home";
@@ -7,6 +7,7 @@ import Register from "../pages/register";
 export const AppRouter = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/home" element={<Home />} />
