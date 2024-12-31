de83f6973122782a92117af499f16694bf127e08
Paulo Zanluqui <paulozanluqui13@gmail.com>
2024-12-31 00:02:15 -0300

    fix: Ajuste no defaultValue do component do Input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -7,14 +7,15 @@ interface InputProps {
   placeholder: string
   name: string
   value?: any;
+  defaultValue?: any
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export function Input({ Icon, type, placeholder, name, value = "", onChange }: InputProps) {
+export function Input({ Icon, type, placeholder, name, value, defaultValue, onChange }: InputProps) {
   return (
     <div className="h-14 px-4 bg-gray-50 border-gray-300 dark:bg-gray-700 dark:border-gray-600 rounded-lg flex items-center gap-2">
         <Icon className="size-5 text-zinc-400" />
-        <input onChange={onChange} name={name} placeholder={placeholder} type={type} value={value} className="bg-transparent text-lg placeholder-zinc-400 dark:text-white outline-none flex-1" />
+        <input onChange={onChange} name={name} placeholder={placeholder} type={type} value={value} defaultValue={defaultValue} className="bg-transparent text-lg placeholder-zinc-400 dark:text-white outline-none flex-1" />
     </div>
   );
 }
