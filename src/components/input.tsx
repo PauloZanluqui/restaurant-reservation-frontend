36cebaee54f63a93360426d31e5c8f2ae153a5a9
Paulo Zanluqui <paulozanluqui13@gmail.com>
2024-12-30 23:42:22 -0300

    feat: Possibilidade do input receber um valor padrão

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -6,14 +6,15 @@ interface InputProps {
   type: string,
   placeholder: string
   name: string
+  value?: any;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export function Input({ Icon, type, placeholder, name, onChange }: InputProps) {
+export function Input({ Icon, type, placeholder, name, value = "", onChange }: InputProps) {
   return (
     <div className="h-14 px-4 bg-gray-50 border-gray-300 dark:bg-gray-700 dark:border-gray-600 rounded-lg flex items-center gap-2">
         <Icon className="size-5 text-zinc-400" />
-        <input onChange={onChange} name={name} placeholder={placeholder} type={type} className="bg-transparent text-lg placeholder-zinc-400 dark:text-white outline-none flex-1" />
+        <input onChange={onChange} name={name} placeholder={placeholder} type={type} value={value} className="bg-transparent text-lg placeholder-zinc-400 dark:text-white outline-none flex-1" />
     </div>
   );
 }
